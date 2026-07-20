217c5695273359ce9931731f74b85ec02314c7ce
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 23:27:35 +0300

    add Roadmap Preview section

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,16 @@ import Hero from "@/components/home/Hero";
 import FeaturedProject from "@/components/home/FeaturedProject";
 import LatestInsights from "@/components/home/LatestInsights";
 import UniversityPreview from "@/components/home/UniversityPreview";
+import RoadmapPreview from "@/components/home/RoadmapPreview";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
-
       <FeaturedProject />
-
       <LatestInsights />
-
       <UniversityPreview />
+      <RoadmapPreview />
     </main>
   );
 }
