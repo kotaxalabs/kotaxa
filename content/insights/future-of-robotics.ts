14897a2a25e3178a3c7c9661d396bc67714a0cd5
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 23:19:48 +0300

    Create future-of-robotics.ts

---
+++ content/insights/future-of-robotics.ts
@@ -0,0 +1,16 @@
+export const futureOfRobotics = {
+  slug: "future-of-robotics",
+
+  title: "The Future of Robotics",
+
+  description:
+    "From autonomous systems to personal assistants, robotics is entering a new era powered by AI.",
+
+  image: "/images/insights/future-of-robotics.webp",
+
+  category: "Robotics",
+
+  publishedAt: "July 2026",
+
+  href: "/insights/future-of-robotics",
+};
