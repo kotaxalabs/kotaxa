51f26ed8c7fb30b0d6607f3bef6672f9338e656a
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 23:30:26 +0300

    add responsive footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
 import "./globals.css";
 
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-[#09090B] text-white antialiased">
         <Header />
 
         {children}
+
+        <Footer />
       </body>
     </html>
   );
