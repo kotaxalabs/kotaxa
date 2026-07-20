3977db7838f86c15d760f6c7d4634384a4c8bc91
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 23:15:24 +0300

    Create ai-vs-traditional-development.ts

---
+++ content/insights/ai-vs-traditional-development.ts
@@ -0,0 +1,19 @@
+export const article = {
+
+slug:"ai-vs-traditional-development",
+
+title:"AI vs Traditional Development",
+
+description:
+"How AI is changing modern software engineering.",
+
+cover:
+"/images/insights/ai-development.webp",
+
+date:"2026",
+
+category:"Artificial Intelligence",
+
+href:"/insights/ai-vs-traditional-development"
+
+}
