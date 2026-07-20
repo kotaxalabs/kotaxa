dde6cdba48763683edc87b509c93442650fab570
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 23:11:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/home/Hero";
+import FeaturedProject from "@/components/home/FeaturedProject";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <FeaturedProject />
     </main>
   );
 }
