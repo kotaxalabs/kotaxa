797a4b4e476174139089b3b046ec70b1b7a02ffd
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 23:55:47 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,18 @@ import "./globals.css";
 
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import PageBackground from "@/components/layout/PageBackground";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className="bg-[#09090B] text-white antialiased">
+        <PageBackground />
+
         <Header />
 
         {children}
