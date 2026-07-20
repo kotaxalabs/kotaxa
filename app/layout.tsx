7e29a98a6565d6c9efd2abb8cf65e08b6c7e062c
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 22:51:58 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import "./globals.css";
 
+import Header from "@/components/layout/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+
+        {children}
+      </body>
+    </html>
+  );
+}
