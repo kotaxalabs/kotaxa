6f52a522d7f8fd6e2f06eb7928520f161c82a2b1
kotaxalabs <kotaxalabs@gmail.com>
2026-07-20 22:45:33 +0300

    Create navigation.ts

---
+++ components/navigation/navigation.ts
@@ -0,0 +1,30 @@
+export const navigation = [
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    label: "Insights",
+    href: "/insights",
+  },
+  {
+    label: "University",
+    href: "/university",
+  },
+  {
+    label: "Roadmap",
+    href: "/roadmap",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+  {
+    label: "Support",
+    href: "/support",
+  },
+];
